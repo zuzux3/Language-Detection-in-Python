2586a0da4718b909d640c06eb1938fb1723bd664
zuzux3 <zuzannatarazewicz@gmail.com>
2021-10-21 01:16:16 +0200

    Some bugs are here but I'll work it through tomorrow

--- script.py
+++ script.py
@@ -4,13 +4,42 @@ from re import *
 from seaborn import *
 from matplotlib.pyplot import *
 from warnings import *
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 simplefilter('ignore')
 
 data = read_csv("X:\projects\Language Detection in Python\Language Detection.csv")
-data.head(10)
 data["Language"].value_counts()
 
-x = data["Text"]
+X = data["Text"]
 y = data["Language"]
+le = LabelEncoder()
+y = le.fit_transform(y)
 
+dataList = []
+
+for text in X:
+    text = sub(r"[!@#$(),n\"%?:;~`0-9]", r' ', text)
+    text = sub(r'[[]]', r' ', text)
+    text = text.lower()
+    dataList.append(text)
+
+cv = CountVectorizer()
+X = cv.fit_transform(dataList).toarray()
+X.shape
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+model = MultinomialNB()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+ac = accuracy_score(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)
+
+print("Accuracy is: ", ac)
